5dfe2830c0f9aa768ddac144a0caf803d9f9349a
josue riera <josueriera23@hotmail.com>
2023-06-19 15:47:49 -0500

    4

--- Estrucura.c
+++ Estrucura.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
 struct hola
-    {
-        char nombre[20];
-        char direcion[20];
-        char carrera[20];
-        int edad;
-        float promedio;
-    };
+{
+    char nombre[20];
+    char direcion[20];
+    char carrera[20];
+    int edad;
+    float promedio;
+};
 
 int main()
 {
     struct hola alumnos[5];
-    
-    for (int i = 0; i < 5; i++) {
-        printf("Alumno %d\n", i+1);
-        
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Alumno %d\n", i + 1);
+
         printf("Nombre: ");
         gets(alumnos[i].nombre);
-        fflush(stdin);  // Limpiar el búfer del teclado
-        
+        fflush(stdin); // Limpiar el búfer del teclado
+
         printf("Edad: ");
         scanf("%d", &alumnos[i].edad);
-        fflush(stdin);  // Limpiar el búfer del teclado
-        
+        fflush(stdin); // Limpiar el búfer del teclado
+
+        printf("Direcion: ");
+        gets(alumnos[i].direcion);
+        fflush(stdin); // Limpiar el búfer del teclado
+
+        printf("Carrera: ");
+        gets(alumnos[i].carrera);
+        fflush(stdin); // Limpiar el búfer del teclado
+
         printf("Promedio: ");
         scanf("%f", &alumnos[i].promedio);
-        fflush(stdin);  // Limpiar el búfer del teclado
-        
+        fflush(stdin); // Limpiar el búfer del teclado
         printf("\n");
     }
+printf("Datos de los alumnos:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Alumno %d\n", i+1);
+        printf("Nombre: %s\n", alumnos[i].nombre);
+        printf("Nombre: %s\n", alumnos[i].carrera);
+        printf("Nombre: %s\n", alumnos[i].direcion);
+        printf("Edad: %d\n", alumnos[i].edad);
+        printf("Promedio: %.2f\n", alumnos[i].promedio);
 
-
+        printf("\n");
+    }
     return 0;
 }
