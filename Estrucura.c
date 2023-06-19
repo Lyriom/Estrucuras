81016205e9790e5d7869e68ac33726b2bf8530f0
josue riera <josueriera23@hotmail.com>
2023-06-19 15:18:09 -0500

    2

--- Estrucura.c
+++ Estrucura.c
@@ -2,12 +2,20 @@
 
 int main()
 {
-struct hola
-{
-    /* data */
-};
-
-    printf("Hello world");
+    struct hola
+    {
+        char nombre[20];
+        char direcion[20];
+        char carrera[20];
+        int edad;
+        float promedio;
+    } a1 = {"juan", "centro", "Software", 20, 9.1};
+    printf("%s", a1.nombre);
+    printf("los datos de estudiante 1 son \n");
+    printf("%s  \n", a1.direcion);
+    printf("%s  \n", a1.carrera);
+    printf("%d  \n", a1.edad);
+    printf("%.3f  \n", a1.promedio);
 
     return 0;
 }
